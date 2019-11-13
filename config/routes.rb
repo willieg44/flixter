15389f3e140429c128f492387ed6b3421030476e
willieg44@gmail.com <willieg44@gmail.com>
2019-11-13 01:54:18 +0000

    building lessons

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+        resources :sections, only: [:new, :create]
     end
   end
 end
