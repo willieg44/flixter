2f6f4e02a7470fec61a1d912ae3133145ee32446
willieg44@gmail.com <willieg44@gmail.com>
2019-11-14 02:42:15 +0000

    updated the students section of the web page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :instructor do
     resources :sections, only: [] do
