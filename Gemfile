0ed4bba432f020a2342a96996ee27469791beadc
willieg44@gmail.com <willieg44@gmail.com>
2019-11-19 02:35:32 +0000

    added photo uploader

--- Gemfile
+++ Gemfile
@@ -69,3 +69,4 @@ source 'https://rails-assets.org' do
 end
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_010546) do
+ActiveRecord::Schema.define(version: 2019_11_15_005657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,14 @@ ActiveRecord::Schema.define(version: 2019_11_13_010546) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", force: :cascade do |t|
