bdaab6283afa8b818d661d6e90ff069885424c03
willieg44@gmail.com <willieg44@gmail.com>
2019-12-01 23:14:05 +0000

    added the student to go to the next part of the lesson with a trigger button, also added to if the secion is blank the student will be directed to the next lesson or section in order

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -4,4 +4,9 @@ class Lesson < ApplicationRecord
 
     include RankedModel
     ranks :row_order, with_same: :section_id
+
+    def next_lesson
+        lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+        return lesson
+    end
 end
