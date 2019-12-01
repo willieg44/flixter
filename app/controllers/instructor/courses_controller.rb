e124217fa1d25323a0299947a5e2cbb2abe41f96
willieg44@gmail.com <willieg44@gmail.com>
2019-12-01 21:55:07 +0000

    added a new simplier way for instructors to add a new course to lesson

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -14,6 +14,7 @@ class Instructor::CoursesController < ApplicationController
     end
 
     def show
+        @section = Section.new
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ end
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
+        resources :sections, only: [:create]
     end
   end
 end
